9688ca390d5ccfeff85b00ff194f65bcf0de2695
Your Name <you@example.com>
2025-03-26 09:53:14 +0530

    ud

--- frontend/app/dss/home/page.tsx
+++ frontend/app/dss/home/page.tsx
@@ -1,9 +1,14 @@
 import GridSection from './component/home_grid/GridSection';
 import GalleryCarousel from './component/project_images/GalleryCarousel';
+import StepCardsGrid from './component/cards/StepCards.Grid'; 
+import SocialGridSection from './component/social/social';
+
 export default function Home() {
     return(<div>
       <GridSection/>
       <GalleryCarousel/>
-      main home page Page Content</div>);
+      <StepCardsGrid/>
+      <SocialGridSection/>
+      </div>); 
   }
   
